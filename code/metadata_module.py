f85ec96e7cc770217ce93b360ef84f18a9cc96ef
Youssef <Youssef@IMG-Omega.local>
2022-02-28 13:44:04 +0100

    Done metadata extraction

--- code/metadata_module.py
+++ code/metadata_module.py
@@ -6,24 +6,58 @@ Created on Thu Feb 17 13:49:45 2022
 
 This module aims to read and extract metadata from -almost- any given image
 format using bioformats package for python.
+To extract the needed metadata, follow the bioformat scehema documentation (
+https://www.openmicroscopy.org/Schemas/Documentation/ \
+    Generated/OME-2016-06/ome.html)
+
 Note that python-bioformats package uses python-javabridge to start and
 interact with a Java virtual machine.
-https://pythonhosted.org/python-bioformats/
 """
 
-""" Waiting until installing jdr locally
-pip install javabridge
-pip install python-bioformats
 
-import numpy as np
+"""
+Mandatory commands: (from https://pythonhosted.org/python-bioformats/)
+    pip install javabridge
+    pip install python-bioformats
+
+Remark: Code tested only on google colab
+"""
+
+
 import javabridge
 import bioformats
 javabridge.start_vm(class_path=bioformats.JARS)
 
 
-path_coloc = "/Users/Youssef/Documents/IBDML/Data/homogeneity/lame/homogeneite10zoom06-405.lsm"
-coloc_xml = bioformats.get_omexml_metadata(path=path_coloc)
-coloc_omexml = bioformats.OMEXML(coloc_xml)
+def get_tiff_omexml(tiff_path):
+    """
+    This function returns the metadata of the tiff file.
+    The metadata is stored in a bioformats specific format.
+    Information from the metadata will be extracted then -from the returned
+    variable- following bioformats schema documentation.
+
+    Parameters
+    ----------
+    tiff_path : str
+        path of the tiff file.
+
+    Returns
+    -------
+    tiff_omexml : bioformats.omexml.OMEXML
+        metadata of the chosen tiff file.
+
+    """
+    tiff_xml = bioformats.get_omexml_metadata(path=tiff_path)
+    tiff_omexml = bioformats.OMEXML(tiff_xml)
+    return tiff_omexml
+
+
+"""
+# get sample name
+path_homo = "/Users/Youssef/Documents/IBDML/Data/homogeneity/lame/" + \
+    "homogeneite10zoom1-488.tif"
+get_tiff_omexml(tiff_path=homo_path).image().Name
+
+# print the different information labels
 dir(coloc_omexml)
-print(coloc_omexml.image().Name)
 """
